82d8021de9fffc1900377a77241ea744e0f54b77
Abdul Hadi <hadiulfaisal@gmail.com>
2025-09-19 12:58:10 +0500

    Initial Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,26 @@ export const metadata: Metadata = {
   description: DATA.description,
   icons: {
     icon: [
-      { url: "/mine.png", sizes: "any" },
-      { url: "/mine.png", sizes: "32x32", type: "image/png" },
-      { url: "/mine.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    shortcut: "/mine.png",
-    apple: "/mine.png",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "android-chrome-512x512", 
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
   },
   openGraph: {
     title: `${DATA.name}`,
@@ -64,6 +78,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
